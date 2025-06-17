b92baac711597ba3f2af9ab25b190041db69c707
zurira <20243ds036@utez.edu.mx>
2025-06-16 20:12:45 -0600

    Realice el contener del login, de igual manera realice la simulación de un logo de usuario de forma circular, agregue el titulo y agrgue estos elementos al panel

--- login/src/main/java/mx/edu/utez/inicio/HelloApplication.java
+++ login/src/main/java/mx/edu/utez/inicio/HelloApplication.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 
@@ -19,6 +20,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage)  {
+        //Imagen de fondo
         Image backgroundImage = new Image (getClass().getResourceAsStream("/images/fondo.jpg"));
         BackgroundImage background = new BackgroundImage(
                 backgroundImage,
@@ -30,6 +32,32 @@ public class HelloApplication extends Application {
 
         StackPane root = new StackPane();
         root.setBackground(new Background(background));
+
+        //Contenedor del login
+        VBox loginCard = new VBox();
+        loginCard.setPadding(new Insets(30));
+        loginCard.setAlignment(Pos.CENTER);
+        loginCard.setMaxWidth(300);
+        loginCard.setStyle("-fx-background-color: rgba(255,255,255,0.9); -fx-background-radius: 20;");
+
+        //Logo de forma circular
+        Circle logo = new Circle();
+        logo.setFill(Color.LIGHTPINK);
+        Label lblLogo = new Label();
+        lblLogo.setTextFill(Color.GRAY);
+        StackPane logoContainer = new StackPane(logo, lblLogo);
+
+        //Titulo
+        Label title = new Label("INICIO DE SESIÓN");
+        title.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+        title.setTextFill(Color.GRAY);
+
+        // Agregar elementos al panel
+        loginCard.getChildren().addAll(
+                logoContainer, title
+        );
+
+        root.getChildren().add(loginCard);
 
         Scene scene = new Scene(root, 700, 450);
         stage.setTitle("Inicio de sesión biblioteca");
