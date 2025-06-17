4dd393265d17eb21915430412739df548a9c1e73
TaniaSR8 <20243ds011@utez.edu.mx>
2025-06-16 21:58:56 -0600

    Finalizacion de codigo

--- src/main/java/com/ejemplo/libreria/MainApp.java
+++ src/main/java/com/ejemplo/libreria/MainApp.java
@@ -10,13 +10,12 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
         Parent root = FXMLLoader.load(getClass().getResource("/com/ejemplo/libreria/resgistro.fxml"));
         primaryStage.setTitle("Registro de Usuario");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
+//finalizacio
     public static void main(String[] args) {
         launch(args);
     }
